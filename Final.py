c62f61aeec3e52bc2d94919a41d0675f79173381
Che-tsun Ho <republicho@foxmail.com>
2022-05-25 16:50:46 +0800

    Update Final and README.md

--- Final.py
+++ Final.py
@@ -1,5 +1,7 @@
 import io
 import os
+import re
+import sys
 import queue
 import time
 import wave
@@ -9,6 +11,10 @@ import serial.tools.list_ports
 import pyaudio
 from pygame import mixer
 from concurrent.futures import ThreadPoolExecutor
+from google.cloud import speech
+from google.cloud import texttospeech
+from __future__ import division
+from six.moves import queue
 
 Flag = True
 Connected = False
@@ -17,7 +23,12 @@ Connected = False
 RATE = 16000
 CHUNK = int(RATE/10) # 100ms
 
-class Text2Speech(object):
+
+def Text2Speech(text):
+    """Synthesizes speech from the input string of text."""
+    client = texttospeech.TextToSpeechClient()
+
+class Speech2Text(object):
     '''Opens a recording stream as
     a generator yielding the audio'''
     def __init__(self, rate, chunk):
@@ -27,6 +38,90 @@ class Text2Speech(object):
         # Create a thread-safe buffer of audio data
         self_buff = queue.Queue()
         self.closed = True
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream,
+        into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b"".join(data)
+
+def listen_print_loop(responses):
+    """ Iterates through server responses and prints them.
+
+        The responses passed is a generator that will block until a response
+        is provided by the server.
+
+        Each response may contain multiple results, and each result may contain
+        multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+        print only the transcription for the top alternative of the top result.
+
+        In this case, responses are provided for interim results as well. If the
+        response is an interim one, print a line feed at the end of it, to allow
+        the next result to overwrite it, until the response is a final one. For the
+        final one, print a newline to preserve the finalized transcription.
+        """
+    num_chars_printed = 0
+    for response in responses:
+        if not response.results:
+            continue
+
+        # The `results` list is consecutive. For streaming, we only care about
+        # the first result being considered, since once it's `is_final`, it
+        # moves on to considering the next utterance.
+        result = response.results[0]
+        if not result.alternatives:
+            continue
 
 
 class PlayMP3():
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # PhoneCallSIM800C
 It implements the Phone Call using ATK-SIM800C GSM/GPRS Module. 
-It deals with socket programming, audio processing and OOP in Python, integrated with SIM800C module. 
+Moreover, Text2Speech and Speech2Text will be integrated. 
+It deals with multiple scope, like serial programming, audio processing, and OOP knowledge.
+More details will be available later.
+
+## Setting up a Python development environment
+Always remember to use a pre-project environment when developing locally with Python. Please refer to Google Cloud Guide (https://cloud.google.com/python/docs/setup#windows) to set up virtual environment. 
+
+<pre><code>
+
+</code></pre>
