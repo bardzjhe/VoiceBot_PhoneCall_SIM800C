6927ad25b520c047d8466dcfadf7b4c12a86ab19
長崎岸久 <ngoscar9008@gmail.com>
2022-06-01 15:49:17 +0800

    rewrite speech2text function

--- main.py
+++ main.py
@@ -22,9 +22,162 @@ from six.moves import queue
 RATE = 16000
 CHUNK = int(RATE/10) # 100ms
 
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'ServiceAccount.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'demoServiceAccount.json'
 client = texttospeech.TextToSpeechClient()
 speech_client = speech.SpeechClient()
+
+
+class MicrophoneStream(object):
+    """Opens a recording stream as a generator yielding the audio chunks."""
+
+    def __init__(self, rate, chunk):
+        self._rate = rate
+        self._chunk = chunk
+
+        # Create a thread-safe buffer of audio data
+        self._buff = queue.Queue()
+        self.closed = True
+
+    def __enter__(self):
+        self._audio_interface = pyaudio.PyAudio()
+        self._audio_stream = self._audio_interface.open(
+            format=pyaudio.paInt16,
+            # The API currently only supports 1-channel (mono) audio
+            # https://goo.gl/z757pE
+            channels=1,
+            rate=self._rate,
+            input=True,
+            frames_per_buffer=self._chunk,
+            # Run the audio stream asynchronously to fill the buffer object.
+            # This is necessary so that the input device's buffer doesn't
+            # overflow while the calling thread makes network requests, etc.
+            stream_callback=self._fill_buffer,
+        )
+
+        self.closed = False
+
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self._audio_stream.stop_stream()
+        self._audio_stream.close()
+        self.closed = True
+        # Signal the generator to terminate so that the client's
+        # streaming_recognize method will not block the process termination.
+        self._buff.put(None)
+        self._audio_interface.terminate()
+
+    def _fill_buffer(self, in_data, frame_count, time_info, status_flags):
+        """Continuously collect data from the audio stream, into the buffer."""
+        self._buff.put(in_data)
+        return None, pyaudio.paContinue
+
+    def generator(self):
+        while not self.closed:
+            # Use a blocking get() to ensure there's at least one chunk of
+            # data, and stop iteration if the chunk is None, indicating the
+            # end of the audio stream.
+            chunk = self._buff.get()
+            if chunk is None:
+                return
+            data = [chunk]
+
+            # Now consume whatever other data's still buffered.
+            while True:
+                try:
+                    chunk = self._buff.get(block=False)
+                    if chunk is None:
+                        return
+                    data.append(chunk)
+                except queue.Empty:
+                    break
+
+            yield b"".join(data)
+
+def listen_print_loop(responses):
+    """Iterates through server responses and prints them.
+
+    The responses passed is a generator that will block until a response
+    is provided by the server.
+
+    Each response may contain multiple results, and each result may contain
+    multiple alternatives; for details, see https://goo.gl/tjCPAU.  Here we
+    print only the transcription for the top alternative of the top result.
+
+    In this case, responses are provided for interim results as well. If the
+    response is an interim one, print a line feed at the end of it, to allow
+    the next result to overwrite it, until the response is a final one. For the
+    final one, print a newline to preserve the finalized transcription.
+    """
+    num_chars_printed = 0
+    for response in responses:
+        if not response.results:
+            continue
+
+        # The `results` list is consecutive. For streaming, we only care about
+        # the first result being considered, since once it's `is_final`, it
+        # moves on to considering the next utterance.
+        result = response.results[0]
+        if not result.alternatives:
+            continue
+
+        # Display the transcription of the top alternative.
+        transcript = result.alternatives[0].transcript
+
+        # Display interim results, but with a carriage return at the end of the
+        # line, so subsequent lines will overwrite them.
+        #
+        # If the previous result was longer than this one, we need to print
+        # some extra spaces to overwrite the previous result
+        overwrite_chars = " " * (num_chars_printed - len(transcript))
+
+        if not result.is_final:
+            sys.stdout.write(transcript + overwrite_chars + "\r")
+            sys.stdout.flush()
+
+            num_chars_printed = len(transcript)
+
+        else:
+            print(transcript + overwrite_chars)
+
+            # Exit recognition if any of the transcribed phrases could be
+            # one of our keywords.
+            if re.search(r"\b(exit|quit)\b", transcript, re.I):
+                print("Exiting..")
+                break
+
+            num_chars_printed = 0
+
+def speech2text():
+    # See http://g.co/cloud/speech/docs/languages
+    # for a list of supported languages.
+    primary_language = "yue-Hant-HK"  # a BCP-47 language tag
+    secondary_language1 = "en-US"
+    secondary_language2 = "zh"
+
+    client = speech.SpeechClient()
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.LINEAR16,
+        sample_rate_hertz=RATE,
+        language_code=primary_language,
+        alternative_language_codes=[secondary_language1, secondary_language2]
+    )
+
+    streaming_config = speech.StreamingRecognitionConfig(
+        config=config, interim_results=True
+    )
+
+    with MicrophoneStream(RATE, CHUNK) as stream:
+        audio_generator = stream.generator()
+        requests = (
+            speech.StreamingRecognizeRequest(audio_content=content)
+            for content in audio_generator
+        )
+
+        responses = client.streaming_recognize(streaming_config, requests)
+
+        # Now, put the transcription responses to use.
+        listen_print_loop(responses)
 
 def text2speech(text):
 
@@ -50,31 +203,31 @@ def text2speech(text):
     with open('audio file1.mp3', 'wb') as output:
         output.write(response.audio_content)
 
-def speech2text():
-    ## Step 1. Loadd the media files (Transcribe media files)
-    primary_language = "yue-Hant-HK"  # a BCP-47 language tag
-    secondary_language1 = "en-US"
-    secondary_language2 = "zh"
-    media_file_name_wav = 'temp_audio.wav'
-    with open(media_file_name_wav, 'rb') as f2:
-        byte_data_wav = f2.read()
-    audio_wav = speech.RecognitionAudio(content=byte_data_wav)
+# def speech2text():
+#     ## Step 1. Loadd the media files (Transcribe media files)
+#     primary_language = "yue-Hant-HK"  # a BCP-47 language tag
+#     secondary_language1 = "en-US"
+#     secondary_language2 = "zh"
+#     media_file_name_wav = 'temp_audio.wav'
+#     with open(media_file_name_wav, 'rb') as f2:
+#         byte_data_wav = f2.read()
+#     audio_wav = speech.RecognitionAudio(content=byte_data_wav)
 
-    ## Step 2. Configure Media Files Output
-    config_wav = speech.RecognitionConfig(
-        sample_rate_hertz=44100,
-        enable_automatic_punctuation=True,
-        language_code='en_us',
-        alternative_language_codes=[secondary_language1, secondary_language2],
-        audio_channel_count=2
-    )
+#     ## Step 2. Configure Media Files Output
+#     config_wav = speech.RecognitionConfig(
+#         sample_rate_hertz=44100,
+#         enable_automatic_punctuation=True,
+#         language_code='en_us',
+#         alternative_language_codes=[secondary_language1, secondary_language2],
+#         audio_channel_count=2
+#     )
       
-    ## Step 3. Transcribing the Recognition objects
-    response_standard_wav = speech_client.recognize(
-        config=config_wav,
-        audio=audio_wav
-    )
-    print (response_standard_wav)
+#     ## Step 3. Transcribing the Recognition objects
+#     response_standard_wav = speech_client.recognize(
+#         config=config_wav,
+#         audio=audio_wav
+#     )
+#     print (response_standard_wav)
 
 class PlayMP3():
 
@@ -195,8 +348,9 @@ def calling(phonenum):
                 print("\ndialed")
 
                 # Start audio recording
-                audio_thread = AudiRecorder()
-                executor.submit(audio_thread.start(), )
+                # audio_thread = AudiRecorder()
+                # executor.submit(audio_thread.start(), )
+                executor.submit(speech2text(), )
 
                 # Text to speech
                 text = "Hello. This is Jack. Long Time No See. How is everything going these days. This calling is from Google Cloud Platform using Artificial Intelligence. I wanna ask if Polyu has any room for improvement, please reply and I will record your audio. When you finish your comment, please ring off directly. "
@@ -209,7 +363,7 @@ def calling(phonenum):
             if x.find('NO CARRIER') != -1:
                 print("\nRing off")
                 # Stop audio recording after the end of the call
-                executor.submit(audio_thread.stop)
+                # executor.submit(audio_thread.stop)
                 break
 
             if (x.find('BUSY') != -1) | (x.find('ERROR') != -1) | (x.find('NO ANSWER') != -1): 
@@ -217,10 +371,9 @@ def calling(phonenum):
                 break
 
 def main():
+    calling(56117107)
 
-    calling()
-
-    speech2text()
+    # speech2text()
     print("Done")
     exit()
 
